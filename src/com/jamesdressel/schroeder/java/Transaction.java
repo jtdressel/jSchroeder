1b63877b56a5bef50dd61e56879347f8fe463bab
James Dressel <ubergeek@jamesdressel.com>
2012-05-02 01:12:53 -0400

    CODE: Create barebone Transaction class

---
+++ src/com/jamesdressel/schroeder/java/Transaction.java
@@ -0,0 +1,17 @@
+package com.jamesdressel.schroeder.java;
+import java.util.Set;
+import java.util.Date;
+import java.util.UUID;
+
+/* The transaction class is used to represent a single transaction, such as 
+ * buying groceries, or getting paid. There are at least two SubTransactions
+ * one debiting an account, one crediting an account. In addition there is a 
+ * memo line, date/time,  and guid.
+ */
+public class Transaction {
+	Set subTransactions;
+	String memo;
+	UUID id;
+	Date date;
+	
+}

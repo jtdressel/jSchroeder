8d6c35d6343c27f13e261f9fa9c1499f0b5ebacd
James Dressel <ubergeek@jamesdressel.com>
2012-05-02 22:48:18 -0400

    TEST: Setup account.addTransaction test

--- src/com/jamesdressel/schroeder/java/Account.java
+++ src/com/jamesdressel/schroeder/java/Account.java
@@ -1,14 +1,20 @@
 package com.jamesdressel.schroeder.java;
 
 import java.util.UUID;
+import java.util.Set;
+import java.util.HashSet;
+
 
 public class Account {
 	private UUID id;
 	private String name;
+	private Set<Transaction> transactions;
+	
 	
 	public Account(){
 		setName("");
 		id = UUID.randomUUID();
+		transactions = new HashSet<Transaction>();
 	}
 
 	String getName() {
@@ -23,7 +29,15 @@ public class Account {
 		return id;
 	}
 	
+	public void addTransaction(Transaction trans){
+		
+	}
 	
+	public Set getTransactions(){
+		return null;
+	}
+	
+
 	
 	
 }
---
+++ src/com/jamesdressel/schroeder/java/AccountTest.java
@@ -0,0 +1,63 @@
+package com.jamesdressel.schroeder.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AccountTest {
+	SubTransaction one;
+	SubTransaction two;
+	SubTransaction three;
+	SubTransaction four;
+	Transaction trans;
+	Transaction secondTrans;
+	Account from;
+	Account to;
+	
+	@Before
+	public void initalize(){
+		from = new Account();
+		to = new Account();
+		
+		one = new SubTransaction();
+		two = new SubTransaction();
+		three = new SubTransaction();
+		four = new SubTransaction();
+		
+		one.setAccount(from);
+		two.setAccount(to);
+		
+		three.setAccount(to);
+		four.setAccount(from);
+		
+		one.setAmount(-32);
+		two.setAmount(32);
+		three.setAmount(9);
+		four.setAmount(-9);
+		
+		
+		
+		trans = new Transaction();
+		trans.addSubTransaction(one);
+		trans.addSubTransaction(two);
+		
+		secondTrans = new Transaction();
+		secondTrans.addSubTransaction(three);
+		secondTrans.addSubTransaction(four);
+		
+		
+	}
+	
+	@Test
+	public void testAddTransaction() {
+		to.addTransaction(trans);
+		
+		assertTrue(to.getTransactions().contains(trans));
+		
+		
+	}
+	
+	
+}
